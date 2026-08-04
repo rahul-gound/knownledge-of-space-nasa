78eb339f22b841928778e4dc4d4382e364b97ca8
rahul gound <want.to.learn.free@gmail.com>
2026-08-04 10:21:28 +0000

    2nd push

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
-import './style.css'
+const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 
-document.querySelector('#app').innerHTML = `
-  <main class="app-shell">
-    <p class="eyebrow">NASA Space Knowledge</p>
-    <h1>Space facts, missions, and discoveries.</h1>
-    <p class="lead">
-      A clean starting point for building out the project without the default Vite starter content.
-    </p>
-  </main>
-`
+document.querySelector("#app").innerHTML = "<p>loading...bro...wait...</p>";
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+  .then(response => response.json())
+  .then(data => {
+    let media;
+
+    if (data.media_type === "image") {
+      media = `<img src="${data.url}"/>`;
+    } else {
+      media = `<video src="${data.url}" controls></video>`;
+    }
+
+    document.querySelector("#app").innerHTML = `
+      <h1>${data.title}</h1>
+      ${media}
+      <p>${data.explanation}</p>
+    `;
+  })
+  .catch(err => {
+    document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
+  });
